a66513f4ef2b56212e51f7616b889ca3808c7b96
Cody Regalado <hakarushokku@gmail.com>
2018-12-26 22:27:03 -0800

    adding base files

---
+++ src/main/java/GameDev/BlogPostService/BlogPostServiceApplication.java
@@ -0,0 +1,14 @@
+package GameDev.BlogPostService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogPostServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogPostServiceApplication.class, args);
+	}
+
+}
+
